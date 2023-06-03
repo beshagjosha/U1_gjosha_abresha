36241e9a288ce0966bcd3daa6e46368643b08c06
Besha Gjosha <97993034+beshagjosha@users.noreply.github.com>
2023-06-03 12:29:40 +0200

    css och js

---
+++ js/quiz.js
@@ -0,0 +1,86 @@
+"use strict";
+
+let quiz_time = false;
+
+function display_quiz_page(username) {
+    quiz_time = true;
+    main_content.innerHTML = "";
+    background.style.backgroundColor = "yellow";
+
+    let username_display = document.createElement("div");
+    main_content.appendChild(username_display);
+    username_display.classList.add("username_display");
+    username_display.innerHTML = `<p>${username}</p>`;
+
+    let logout_button = document.createElement("button");
+    username_display.appendChild(logout_button);
+    logout_button.classList.add("logout_button");
+    logout_button.textContent = "logout";
+    logout_button.addEventListener("click", local_storage_remove_credentials);
+
+    function local_storage_remove_credentials(event) {
+        localStorage.removeItem("credentials");
+        location.reload();
+    }
+
+    quiz_handler();
+}
+
+function quiz_handler() {
+    let dog_image = document.createElement("img");
+    main_content.appendChild(dog_image);
+    dog_image.setAttribute("src", "media/logo.png");
+    dog_image.classList.add("dog_image");
+
+    display_message("Getting random image...");
+
+    question_handler();
+
+    async function question_handler() {
+        let correct_breed = random_number();
+        let prefix = get_image_prefix(ALL_BREEDS[correct_breed].url);
+        let response = await fetch_handler(prefix);
+        let resource = await response.json();
+
+        dog_image.setAttribute("src", resource.message);
+
+        let options = document.createElement("div");
+        options.classList.add("options");
+        main_content.appendChild(options);
+
+        let options_array = [];
+        options_array.push(correct_breed);
+
+        for (let i = 0; options_array.length < 4; i++) {
+            let random_breed = random_number();
+
+            if (options_array.includes(random_breed)) {
+                continue;
+            }
+
+            options_array.push(random_breed);
+            create_option(random_breed);
+        }
+
+
+        create_option(correct_breed);
+
+        function create_option(random_or_correct) {
+            let option_button = document.createElement("button");
+            option_button.classList.add("option");
+            options.appendChild(option_button);
+            option_button.textContent = `${ALL_BREEDS[random_or_correct].name}`;
+            option_button.style.order = random_number();
+
+            if (random_or_correct === correct_breed) {
+                option_button.addEventListener("click", () => display_message("Correct!", "ONE MORE"));
+            } else {
+                option_button.addEventListener("click", () => display_message("Wrong! Try again!", "ONE MORE"));
+            }
+        }
+    }
+}
+
+function random_number() {
+    return Math.floor(Math.random() * ALL_BREEDS.length);
+}
--- js/server_conncetion.js
+++ js/server_conncetion.js
@@ -1,1 +1,81 @@
 "use strict";
+
+let overlay = document.querySelector("#overlay");
+let message = document.querySelector("#message");
+
+let login_success;
+
+async function fetch_handler(URL) {
+    if (quiz_time !== true) {
+        display_message("Contacting Server ...", "CLOSE");
+    }
+
+    let response = await fetch(URL);
+
+    remove_message();
+
+
+    if (response.status === 418) {
+        display_message("The server thinks it's not a teapot!", "CLOSE");
+    } else if (response.status === 409) {
+        display_message("Sorry, the name is taken. Please try with another one", "CLOSE");
+    } else if (
+        (response.status === 404 && login_page === true) ||
+        (response.status === 400 && login_page === true)
+    ) {
+        let text = document.querySelector("#text");
+        text.textContent = "Wrong username or password.";
+        text.classList.add("wrong_cred");
+    } else if (response.status === 200 && reg_page === true) {
+        display_message("Registration Complete. Proced to login.", "CLOSE");
+    } else if (response.status === 200 && login_page === true) {
+        login_success = true;
+    }
+
+    console.log(response);
+    return response;
+}
+
+function display_message(message_text, close_message_text) {
+    overlay.classList.add("overlay");
+
+    message.classList.add("message");
+    message.textContent = message_text;
+
+    if (message_text === "Contacting Server..." || message_text === "Getting random image ...") {
+        return;
+    }
+
+    let message_button = document.createElement("button");
+    message_button.textContent = close_message_text;
+    message_button.classList.add("close_button");
+    message.appendChild(message_button);
+
+    message_button.addEventListener("click", button_clicked);
+
+    if (message_text === "Correct!") {
+        message.style.backgroundColor = "grey";
+    } else {
+        message.style.backgroundColor = "pink";
+    }
+
+    function button_clicked(event) {
+        message.style.backgroundColor = "red";
+
+        if (quiz_time === true) {
+            remove_message();
+
+            let current_login = JSON.parse(localstorge.getItem("credentials"));
+            display_quiz_page(current_login.user_name);
+        } else {
+            remove_message();
+        }
+    }
+}
+
+function remove_message() {
+    overlay.classList.remove("overlay");
+
+    message.innerHTML = "";
+    message.classList.remove("message");
+}
